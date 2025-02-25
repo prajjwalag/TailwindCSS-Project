7c540162cb960a1a96753deed8b1923ba76bcb36
Prajjwal <prajjwal.agrahari16@gmail.com>
2025-02-25 23:08:22 +0530

    Final Changes of the Project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,21 +2,21 @@ import HeaderTestimonial from "./HeaderTestimonial";
 
 const Hero = () => {
   return (
-      <div>
-          <div id="testimonial-container">
-              <HeaderTestimonial text="The best I’ve found." />
-              <HeaderTestimonial text="Way easier than Calendly." />
-              <HeaderTestimonial text="Okay, I’m hooked!" />
+      <div className="mx-6 py-5 text-center md:mx-12 lg:w-6xl  lg:pt-20 lg:mx-auto">
+          <div id="testimonial-container" className="flex gap-6 flex-wrap justify-evenly">
+              <HeaderTestimonial text="The best I’ve found." starsCount={5}/>
+              <HeaderTestimonial text="Way easier than Calendly."starsCount={5} />
+              <HeaderTestimonial text="Okay, I’m hooked!" starsCount={5}/>
           </div>
-          <h1 className="text-amber-400">
-              The modern way to <span>find a time to meet</span>
+          <h1 className="text-[4rem] font-body font-bold mt-6 tracking-tight leading-16 lg:leading-32 lg:mt-10 lg:text-[8rem]">
+              The modern way to <span className="text-red-500">find a time to meet</span>
           </h1>
-          <p>
+          <p className="mt-6 text-gray-300 font-display text-lg lg:text-3xl lg:max-w-4xl lg:mx-auto leading-snug">
               You’ll love it for the flexible controls to keep your calendar
               sane. They’ll love it for the ultra-convenient booking experience.
           </p>
-          <button>Try SavvyCal risk-free</button>
-          <p>Join over 2,000 happy customers</p>
+          <button className="mt-8 cursor-pointer font-medium text-xl text-black bg-yellow-400 px-8 lg:px-10 py-4  rounded-xl  hover:bg-yellow-300 hover:-translate-y-1 transition-all">Try SavvyCal risk-free</button>
+          <p className="text-gray-300 font-body mt-4 mb-50 text-lg">Join over 2,000 happy customers</p>
       </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import logo from "../assets/logo.svg";
+import LinkButton from "./LinkButton";
 
 const Navbar = () => {
     return (
-        <nav className="flex p-6 items-center justify-between">
+        <nav className="flex p-6 items-center justify-between lg:p-8">
             <a href="#" className="w-42">
                 <img src={logo} alt="" />
             </a>
-            <ul id="menu-bar" className="hidden lg:flex">
-                <li id="menu-item">Features</li>
-                <li id="menu-item">Pricing</li>
-                <li id="menu-item">What's New</li>
+            <ul id="menu-bar" className="hidden lg:flex flex-1 mx-12 gap-8 text-lg">
+                <li id="menu-item"><LinkButton text="Features"/></li>
+                <li id="menu-item"><LinkButton text="Pricing"/></li>
+                <li id="menu-item"><LinkButton text="What's new"/></li>
             </ul>
-            <div id="auth-container">
-                <button className="hidden lg:block">Login</button>
-                <button className="cursor-pointer font-bold text-xl text-black bg-yellow-400 text px-6 py-3 rounded-xl">Sign up</button>
+            <div id="auth-container" className="flex gap-8 items-center">
+                <LinkButton text="Login" className="hidden lg:block text-lg"/>
+                <button className="cursor-pointer font-bold text-xl text-black bg-yellow-400 text px-6 py-3 rounded-xl hover:bg-yellow-300 hover:-translate-y-1 transition-all">Sign up</button>
             </div>
         </nav>
     );
